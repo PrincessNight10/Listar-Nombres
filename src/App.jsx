287df80ff0c78c359be4b9610c86872d23466cd2
Cookienator10 <santiagorojas772@gmail.com>
2024-07-10 16:43:03 -0500

    prueba.

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import {Nombres} from './components/Nombres/ Nombres.jsx'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-   
+  <Nombres/>
     </>
   )
 }
